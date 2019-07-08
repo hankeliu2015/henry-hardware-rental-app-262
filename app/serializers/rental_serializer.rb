66d68404ce7f711b19fa5712a91e12f997e41b59
github username <github email address>
2019-07-08 17:32:33 -0400

    add checkout for rental serializer.

--- app/serializers/rental_serializer.rb
+++ app/serializers/rental_serializer.rb
@@ -1,5 +1,5 @@
 class RentalSerializer < ActiveModel::Serializer
-  attributes :id, :tool_id, :start_date, :return_date
+  attributes :id, :tool_id, :start_date, :return_date, :checkout
   belongs_to :tool
   belongs_to :user
 end
