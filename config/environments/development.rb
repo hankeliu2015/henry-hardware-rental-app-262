e517684b3f08c80f804923af9456df9a0780ff93
github username <github email address>
2019-12-28 22:32:38 -0500

    reset smtp config in dev environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,21 +35,37 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :sendmail
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: 'storeappnotifier@gmail.com'}
-
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-  address: 'smtp.gmail.com',
-  port: 587,
-  domain:'@gmail.com',
-  user_name: ENV['EMAIL_USER_NAME'],
-  password: ENV['EMAIL_PASSWORD'],
-  authentication:'plain',
-  enable_starttls_auto: true
-}
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    user_name: ENV["EMAIL_USER_NAME"],
+    password: ENV["EMAIL_PASSWORD"],
+    authentication: 'plain',
+    enable_starttls_auto: true
+   }
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
+#   the following mail config has some issue.
+#   config.action_mailer.delivery_method = :sendmail
+#   config.action_mailer.perform_deliveries = true
+#   config.action_mailer.raise_delivery_errors = true
+#   config.action_mailer.default_options = {from: 'storeappnotifier@gmail.com'}
+#
+#   config.action_mailer.delivery_method = :smtp
+#   config.action_mailer.smtp_settings = {
+#   address: 'smtp.gmail.com',
+#   port: 587,
+#   domain:'@gmail.com',
+#   user_name: ENV["EMAIL_USER_NAME"],
+#   password: ENV["EMAIL_PASSWORD"],
+#   authentication:'plain',
+#   enable_starttls_auto: true
+# }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
